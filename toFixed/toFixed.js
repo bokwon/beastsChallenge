2de26a1fac8c5e5cd768f1a575b1c632838ff4fe
bo kwon <boyoung@whitesky.com>
2017-04-28 13:07:04 -0700

    fix some spacing

--- toFixed/toFixed.js
+++ toFixed/toFixed.js
@@ -5,33 +5,34 @@ function toFixed(numObj, digits) {
 	var roundValueIndex;
 	var roundedValue;
 	var result;
-	
+
 	if (typeof numObj !== 'number') {
 		throw new TypeError("It should pass number object for the first argument.");
 	}
-	
+
 	if (digits && (digits < 0 || digits > 20)) {
 		throw new RangeError("toFixed() digits argument must be between 0 and 20");
 	}
-	
+
 	if (digits) {
 		newDecimalPoint = curDecimalPoint + digits;
 		roundValueIndex = newDecimalPoint + 1;
-		
-		if (curDecimalPoint > 0) { 
+
+		if (curDecimalPoint > 0) {
 			if (numStr[roundValueIndex] && Number(numStr[roundValueIndex]) >= 5 ) {
 				roundedValue = Number(numStr.substr(newDecimalPoint, 1)) + 1;
 				result = numStr.substr(0, curDecimalPoint) + numStr.substr(curDecimalPoint, digits) + roundedValue.toString();
 			} else {
 				result = numStr + addZeros(roundValueIndex-digits+1);
-			}			
+			}
 		} else {
 			result = numStr + '.' + addZeros(digits);
 		}
+
 	} else {
 		newDecimalPoint = curDecimalPoint - 1;
 		roundValueIndex = curDecimalPoint + 1;
-		
+
 		if (curDecimalPoint > 0) {
 			if (numStr[roundValueIndex] && Number(numStr[roundValueIndex]) >= 5) {
 				roundedValue = Number(numStr.substr(newDecimalPoint, 1)) + 1;
@@ -39,19 +40,20 @@ function toFixed(numObj, digits) {
 			} else {
 				result = numStr.substr(0, curDecimalPoint);
 			}
-		}	else {
+		} else {
 			result = numStr;
 		}
+
 	}
 	return result;
 }
 
 function addZeros(digits) {
 	var zeroStr = "";
-	
+
 	while(zeroStr.length < digits) {
 		zeroStr = zeroStr + "0";
 	}
-	
+
 	return zeroStr;
 }
